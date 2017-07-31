77b959d5ed450032926266c48b7f5022ee43dce8
Shiratie Prodhan <shiratiee@gmail.com>
2017-07-31 13:02:49 -0400

    changes

--- bash.js
+++ bash.js
@@ -1,12 +1,14 @@
 var userCommand=require('./commands.js');
+var chalk = require('chalk');
 
 // Output a prompt
-process.stdout.write('prompt > ');
+process.stdout.write(chalk.yellow('prompt > '));
 process.stdin.on('data', function (data) {
     var cmd = data.toString().trim(); // remove the newline
     var allCommands=cmd.split(" ");
     var cmd=allCommands[0];
     var argument=allCommands[1];
+    console.log(argument);
     userCommand[cmd](argument);
     process.stdout.write('\nprompt>');
 });
--- commands.js
+++ commands.js
@@ -1,26 +1,51 @@
 var fs = require('fs');
+var chalk = require('chalk');
 
-var pwd=function() {
-    process.stdout.write(process.argv[1]);
+var pwd=function(param) {
+    process.stdout.write(chalk.green(process.argv[1]));
 }
     
 
-var ls=function() {
-    console.log(fs.readdirSync('/Users/ErikaNewland/Fullstack/GH1707/node-shell'));
+var ls=function(param) {
+    console.log(chalk.green(fs.readdirSync('/Users/ShiraPro/node-shell/node-shell')));
 }
 
 var echo=function(input){
     if(input==='$PATH'){
         input=process.env.PATH
     }
-    console.log(input);
+    console.log(chalk.green(input));
 }
 
+var cat = function (param) {
+    fs.readFile(param, function (error,file) {
+        if(error) {
+            console.log(error);
+        } else {
+            console.log(file.toString());
+        }
+    });
+}
 
-
+var head = function (param) {
+    fs.readFile(param, function (error,file) {
+        if(error) {
+            console.log(error);
+        } else {
+            var outputarray=file.toString().split('\n');
+            console.log(outputarray);
+            for (var i=0; i<5; i++) {
+                console.log(outputarray[i]);
+            }
+        }
+    });
+}
 
 module.exports={
     pwd: pwd,
     ls: ls,
-    echo: echo
+    echo: echo,
+    cat:cat,
+    head:head
+
 }
